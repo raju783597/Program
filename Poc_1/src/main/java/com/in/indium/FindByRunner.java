27ee72ef71aa6e7c14fceb839dec9620e2192fe3
DT271 <DT271@IND-CHN-LT10844.indium.com>
2021-05-12 15:02:23 +0530

    33

---
+++ Poc_1/src/main/java/com/in/indium/FindByRunner.java
@@ -0,0 +1,13 @@
+package com.in.indium;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class FindByRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Poc_1/src/main/java/com/in/indium/Poc1Application.java
@@ -0,0 +1,14 @@
+package com.in.indium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1Application.class, args);
+	System.out.println("h!! this is Boot");
+	}
+
+}
