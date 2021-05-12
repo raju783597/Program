f48ecee73ffa4eb485a5a330096ed5c2fefb7798
DT271 <DT271@IND-CHN-LT10844.indium.com>
2021-05-12 14:38:59 +0530

    26

---
+++ Poc_6_Feign_Producer_Vendor/src/main/java/com/in/indium/controller/rest/VendorRestController.java
@@ -0,0 +1,19 @@
+package com.in.indium.controller.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/vendor")
+public class VendorRestController {
+	
+	@Value("${server.port}")
+	private int port; 
+	
+	@GetMapping("/data")
+	public String getVendorData() {
+		return "FORM-VENDOR"+port;
+		
+	}
+}
