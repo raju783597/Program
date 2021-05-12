e3f6584f285e4cdd26cd84e960ab1f193fa7d31e
DT271 <DT271@IND-CHN-LT10844.indium.com>
2021-05-12 14:49:51 +0530

    28

---
+++ Poc_8_JMS_Producer/src/main/java/com/in/indium/Poc8JmsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.in.indium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc8JmsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc8JmsProducerApplication.class, args);
+	}
+
+}
