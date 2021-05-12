3c29b67d95844847d70bed06fa69eab3103a36ec
DT271 <DT271@IND-CHN-LT10844.indium.com>
2021-05-12 14:19:38 +0530

    14

---
+++ Poc_27_Spring_DynamicPath3/src/main/java/com/in/indium/controller/ClientController.java
@@ -0,0 +1,11 @@
+package com.in.indium.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web")
+public class ClientController {
+	
+	
+}
