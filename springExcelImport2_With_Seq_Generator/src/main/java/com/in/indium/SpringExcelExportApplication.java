58a9fc62aef770b6a185cc7914f83a19099c74b1
DT271 <DT271@IND-CHN-LT10844.indium.com>
2021-05-12 14:53:48 +0530

    31

---
+++ springExcelImport2_With_Seq_Generator/src/main/java/com/in/indium/SpringExcelExportApplication.java
@@ -0,0 +1,16 @@
+package com.in.indium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringExcelExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExcelExportApplication.class, args);
+	}
+
+
+
+}
